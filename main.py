95a1d2f6b1f8c1c7c85728c30cd873203f84152f
Samuel Whittenberger <sbw978@gmail.com>
2023-04-20 10:04:46 -0400

    add commands and cleanup

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import config
 from datetime import datetime
 
 class chat:
-  def __init__(self, debug):
+  def __init__(self, debug, save):
     openai.organization = config.ORG_ID
     openai.api_key = config.API_KEY
 
     self.debug = debug
+    self.save = save
     self.prompt = ''
     self.system_prompt = 'You are a friendly conversationalist'
     self.msgs = [{'role':'system', 'content':self.system_prompt}]
@@ -18,8 +19,8 @@ class chat:
     )
     self.token_amnts = [res['usage']['total_tokens']]
 
-  def saveConvo(self):
-    if len(self.msgs) == 1:
+  def saveConvo(self) -> None:
+    if not self.save or len(self.msgs) == 1:
       return
     d = datetime.now()
     d_str = str(datetime.date(d)) + '_' + str(datetime.time(d).hour) + '-' + str(datetime.time(d).minute)
@@ -29,23 +30,38 @@ class chat:
         fout.write(': ')
         fout.write(m['content'])
         fout.write('\n')
+  
+  def processPrompt(self, prompt) -> bool:
+    if prompt == ':q':
+        return False
+    elif prompt.split(' ')[0] == ':system':
+      self.system_prompt = prompt.split(' ')[1]
+      print('\n')
+      return True
+    elif prompt == ':debug' or prompt == ':d':
+      self.debug = not self.debug
+      print('Debug on' if self.debug else 'Debug off')
+      return True
+    elif prompt == ':save' or prompt == ':s':
+      self.save = not self.save
+      print('Saving on' if self.save else 'Saving off')
+      return True
 
-  def run(self):
+  def run(self) -> bool:
     try:
       if self.debug:
         print('DEBUG> tokens: ' + str(sum(self.token_amnts)))
         print('DEBUG> system: ' + self.system_prompt)
 
       prompt = input(">")
-      if prompt == 'q':
-        return False
-      elif prompt.split(':')[0] == 'system':
-        self.system_prompt = prompt.split(':')[1]
+      if not prompt:
         print('\n')
         return True
+      if prompt[0] == ':':
+        return self.processPrompt(prompt)
       
       self.msgs.append({"role":"user", "content":prompt})
-      print("\n")
+      print('\n')
 
       res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -68,13 +84,20 @@ class chat:
     except KeyboardInterrupt:
       self.saveConvo()
 
+def handleArgs(argv) -> dict:
+  args = {'debug': False, 'save': False}
+  for a in argv:
+    if a == '-d':
+      args['debug'] = True
+    elif a == '-s':
+      args['save'] = True
+  return args
+
+
 if __name__ == "__main__":
-  debug = False
-  if len(sys.argv) > 1:
-    debug = True if sys.argv[1] == '-d' else False
+  args = handleArgs(sys.argv)
 
-  c = chat(debug)
-
+  c = chat(args['debug'], args['save'])
   while c.run():
     pass
   c.saveConvo()
