6de8d3eb62d801fc51ea76d81b501f7776c8b1a7
Samuel Whittenberger <sbw978@gmail.com>
2023-04-27 10:10:35 -0400

    Bug fix

--- main.py
+++ main.py
@@ -10,113 +10,116 @@ class chat:
 
     self.debug = debug
     self.save = save
-    self.prompt = ''
-    self.system_prompt = 'You are a friendly conversationalist'
-    self.msgs = [{'role':'system', 'content':self.system_prompt}]
-    self.history = self.msgs
+    self.prompt = ""
+    self.system_prompt = "You are a friendly conversationalist"
+
+    self.msgs = [{"role":"system", "content":self.system_prompt}]
+    self.history = []
+    self.history.append(self.msgs[0]["content"])
+
     res = openai.ChatCompletion.create(
-      model='gpt-3.5-turbo',
+      model="gpt-3.5-turbo",
       messages=self.msgs
     )
-    self.token_amnts = [res['usage']['total_tokens']]
+    self.token_amnts = [res["usage"]["total_tokens"]]
 
   def saveConvo(self) -> None:
     if not self.save or len(self.msgs) == 1:
       return
     d = datetime.now()
-    d_str = str(datetime.date(d)) + '_' + str(datetime.time(d).hour) + '-' + str(datetime.time(d).minute)
-    with open("convos/convo_" + d_str + '.txt', "w") as fout:
+    d_str = str(datetime.date(d)) + "_" + str(datetime.time(d).hour) + "-" + str(datetime.time(d).minute)
+    with open("convos/convo_" + d_str + ".txt", "w") as fout:
       for m in self.msgs:
-        fout.write(m['role'])
-        fout.write(': ')
-        fout.write(m['content'])
-        fout.write('\n')
+        fout.write(m["role"])
+        fout.write(": ")
+        fout.write(m["content"])
+        fout.write("\n")
   
   def updateSystemPrompt(self, prompt) -> bool:
-    if len(prompt.split(' ')) < 2:
+    if len(prompt.split(" ")) < 2:
       return False
-    p = prompt[prompt.find(' ')+1:]
+    p = prompt[prompt.find(" ")+1:]
     if p:
       self.system_prompt = p
-      self.msgs.append({'role': 'system', 'content': p})
-      print('System prompt successfully added')
+      self.msgs.append({"role": "system", "content": p})
+      print("System prompt successfully added")
       return True
     return False
   
   def processPrompt(self, prompt) -> bool:
-    if prompt == ':q':
+    if prompt == ":q":
         return False
-    elif prompt.split(' ')[0] == ':system':
+    elif prompt.split(" ")[0] == ":system":
       if not self.updateSystemPrompt(prompt):
-        print('Invalid system prompt')
-      print('\n')
+        print("Invalid system prompt")
+      print("\n")
       return True
-    elif prompt == ':debug' or prompt == ':d':
+    elif prompt == ":debug" or prompt == ":d":
       self.debug = not self.debug
-      print('Debug on' if self.debug else 'Debug off')
+      print("Debug on" if self.debug else "Debug off")
       return True
-    elif prompt == ':save' or prompt == ':s':
+    elif prompt == ":save" or prompt == ":s":
       self.save = not self.save
-      print('Saving on' if self.save else 'Saving off')
+      print("Saving on" if self.save else "Saving off")
       return True
     else:
-      print('Command not recognized\n\n')
+      print("Command not recognized\n\n")
       return True
   
   def sendChat(self, prompt):
     self.msgs.append({"role":"user", "content":prompt})
-    print('\n')
+    print("\n")
     res = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=self.msgs
     )
-    self.history.append(res['choices'][0]['message']['content'])
-    self.token_amnts.append(res['usage']['total_tokens'])
+    self.history.append(res["choices"][0]["message"]["content"])
+    self.token_amnts.append(res["usage"]["total_tokens"])
 
     return res
 
   def run(self) -> bool:
     try:
       if self.debug:
-        print('DEBUG> tokens: ' + str(sum(self.token_amnts)))
-        print('DEBUG> system: ' + self.system_prompt)
+        print("DEBUG> tokens: " + str(sum(self.token_amnts)))
+        print("DEBUG> system: " + self.system_prompt)
 
       prompt = input(">")
       if not prompt:
-        print('\n')
+        print("\n")
         return True
-      if prompt[0] == ':':
+      if prompt[0] == ":":
         return self.processPrompt(prompt)
       
-      chat_res = self.sendChat(prompt)['choices'][0]['message']['content']
+      chat_res = self.sendChat(prompt)["choices"][0]["message"]["content"]
 
-      # ensure the chat call doesn't exceed max tokens
+      # ensure the chat call doesn"t exceed max tokens
       while sum(self.token_amnts) > 2048:
         self.token_amnts.pop(0)
         self.msgs.pop(0)
 
-      self.msgs.append({'role':'assistant', 'content':chat_res})
+      self.msgs.append({"role":"assistant", "content":chat_res})
       print(chat_res)
-      print('\n')
+      print("\n")
 
       return True
     except KeyboardInterrupt:
       self.saveConvo()
 
 def handleArgs(argv) -> dict:
-  args = {'debug': False, 'save': False}
+  args = {"debug": False, "save": False}
   for a in argv:
-    if a == '-d':
-      args['debug'] = True
-    elif a == '-s':
-      args['save'] = True
+    if a == "-d":
+      args["debug"] = True
+    elif a == "-s":
+      args["save"] = True
   return args
 
 
 if __name__ == "__main__":
   args = handleArgs(sys.argv)
 
-  c = chat(args['debug'], args['save'])
+  c = chat(args["debug"], args["save"])
   while c.run():
     pass
   c.saveConvo()
